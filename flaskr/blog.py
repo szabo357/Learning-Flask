12c412538abe86e7972d076a6b43cf7b390b489a
szabo357 <szabo357@gmail.com>
2023-11-17 18:01:03 -0600

    added get_post to blog.py and url rule to init.py

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -23,3 +23,34 @@ def index():
         " ORDER BY created DESC"     
     ).fetchall()
     return render_template('blog/index.html',posts=posts)
+
+
+def get_post(id, check_author=True):
+    """Get a post and its author by id.
+
+    Checks that the id exists and optionally that the current user is
+    the author.
+
+    :param id: id of post to get
+    :param check_author: require the current user to be the author
+    :return: the post with author information
+    :raise 404: if a post with the given id doesn't exist
+    :raise 403: if the current user isn't the author
+    """
+    post = (
+        get_db()
+        .execute(
+            "SELECT p.id, title, body, created, author_id, username"
+            "FROM post p JOIN user u ON p.author_id = u.id"
+            "WHERE p.id = ?",
+            (id,),
+        )
+    )
+
+    if post is None:
+        abort(404,f"Post id {id} doesn't exist.")
+
+    if check_author and post['author_id'] != g.user["id"]:
+        abort(403)
+    
+    return post    
