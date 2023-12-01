41643e63876dbf3784c18ba7aa7e028506cdf432
szabo357 <szabo357@gmail.com>
2023-12-01 11:58:20 -0600

    admin panel has been added

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -36,9 +36,11 @@ def create_app(test_config=None):
     # and blog blueprints
     from . import auth
     from . import blog
-    
+    from . import admin
+
     app.register_blueprint(auth.bp)
     app.register_blueprint(blog.bp)
+    app.register_blueprint(admin.bp)
 
     # make url_for('index') == url_for('blog.index')
     # in another app, you might define a separate main index here with
